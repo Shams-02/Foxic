67591c1305e5c382348e123955d30526b7c75584
unknown <mianshams6@gmail.com>
2022-08-25 15:05:47 +0500

    Best-Poduct-completed

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        991: {
+            items: 4
+        }
+    }
+})
